faba0118532faeb1a94dc82035aa9c5ad90a7637
rahul357 <rahul40800@gmail.com>
2016-10-01 23:35:38 +0530

    added ar

--- app/src/main/java/com/doomers/hackpaytm/HomeActivity.java
+++ app/src/main/java/com/doomers/hackpaytm/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.doomers.hackpaytm;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -81,7 +82,7 @@ public class HomeActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.Online_shopping) {
-            // Handle the camera action
+           startActivity(new Intent(HomeActivity.this,ProducerConsumerActivity.class));
         }else if(id == R.id.Analytics){
 
         }else if(id == R.id.scan){
