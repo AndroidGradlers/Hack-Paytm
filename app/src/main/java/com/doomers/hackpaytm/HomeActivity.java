de74942e6aff6db41bdcaaad35877d826303a9b9
rahul357 <rahul40800@gmail.com>
2016-10-01 15:37:28 +0530

    minor UI changes

--- app/src/main/java/com/doomers/hackpaytm/HomeActivity.java
+++ app/src/main/java/com/doomers/hackpaytm/HomeActivity.java
@@ -80,17 +80,11 @@ public class HomeActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.Online_shopping) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        }else if(id == R.id.Analytics){
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        }else if(id == R.id.scan){
 
         }
 
