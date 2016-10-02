4318a639264a557b802a4dd074e971404c08116c
gourav <gourav96suri@gmail.com>
2016-10-02 07:53:26 +0530

    Added Home Page, Solved online transaction issue

--- app/src/main/java/com/doomers/hackpaytm/HomeActivity.java
+++ app/src/main/java/com/doomers/hackpaytm/HomeActivity.java
@@ -22,16 +22,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-import com.paytm.pgsdk.PaytmMerchant;
-import com.paytm.pgsdk.PaytmOrder;
-import com.paytm.pgsdk.PaytmPGService;
-import com.paytm.pgsdk.PaytmPaymentTransactionCallback;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -51,7 +42,6 @@ public class HomeActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         context = this;
-        btn = (Button) findViewById(R.id.payButton);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -81,7 +71,7 @@ public class HomeActivity extends AppCompatActivity
                 Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
                 notification.setSound(alarmSound);
 
-                Intent intent1 = new Intent(this,HomeActivity.class);
+                Intent intent1 = new Intent(this,MoneyTransferActivity.class);
                 PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent1,PendingIntent.FLAG_UPDATE_CURRENT);
                 notification.setContentIntent(pendingIntent);
 
@@ -93,220 +83,6 @@ public class HomeActivity extends AppCompatActivity
                 editor.commit();
             }
         }
-
-//        btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                PaytmPGService Service = PaytmPGService.getStagingService();
-//                Map<String, String> paramMap = new HashMap<String, String>();
-//
-//                // these are mandatory parameters
-//
-////                paramMap.put("ORDER_ID", ((EditText) findViewById(R.id.order_id)).getText().toString());
-////                paramMap.put("MID", ((EditText) findViewById(R.id.merchant_id)).getText().toString());
-////                paramMap.put("CUST_ID", ((EditText) findViewById(R.id.customer_id)).getText().toString());
-////                paramMap.put("CHANNEL_ID", ((EditText) findViewById(R.id.channel_id)).getText().toString());
-////                paramMap.put("INDUSTRY_TYPE_ID", ((EditText) findViewById(R.id.industry_type_id)).getText().toString());
-////                paramMap.put("WEBSITE", ((EditText) findViewById(R.id.website)).getText().toString());
-////                paramMap.put("TXN_AMOUNT", ((EditText) findViewById(R.id.transaction_amount)).getText().toString());
-////                paramMap.put("THEME", ((EditText) findViewById(R.id.theme)).getText().toString());
-////                paramMap.put("EMAIL", ((EditText) findViewById(R.id.cust_email_id)).getText().toString());
-////                paramMap.put("MOBILE_NO", ((EditText) findViewById(R.id.cust_mobile_no)).getText().toString());
-//                paramMap.put("ORDER_ID", "20HACK12345");
-//                paramMap.put("MID", "PayAdd32357802476407");
-//                paramMap.put("CUST_ID", "rahul357");
-//                paramMap.put("CHANNEL_ID", "WAP");
-//                paramMap.put("INDUSTRY_TYPE_ID", "Retail");
-//                paramMap.put("WEBSITE", "AddandPay");
-//                paramMap.put("TXN_AMOUNT", "200");
-//                paramMap.put("THEME", "merchant");
-//
-//                PaytmOrder Order = new PaytmOrder(paramMap);
-//
-//                PaytmMerchant Merchant = new PaytmMerchant(
-//                        "https://pguat.paytm.com/paytmchecksum/paytmCheckSumGenerator.jsp",
-//                        "https://pguat.paytm.com/paytmchecksum/paytmCheckSumVerify.jsp");
-//
-//                Service.initialize(Order, Merchant, null);
-//
-//                Service.startPaymentTransaction(context, true, true,
-//                        new PaytmPaymentTransactionCallback() {
-//                            @Override
-//                            public void someUIErrorOccurred(String inErrorMessage) {
-//                                // Some UI Error Occurred in Payment Gateway Activity.
-//                                // // This may be due to initialization of views in
-//                                // Payment Gateway Activity or may be due to //
-//                                // initialization of webview. // Error Message details
-//                                // the error occurred.
-//                            }
-//
-//                            @Override
-//                            public void onTransactionSuccess(Bundle inResponse) {
-//                                // After successful transaction this method gets called.
-//                                // // Response bundle contains the merchant response
-//                                // parameters.
-//                                Log.d("LOG", "Payment Transaction is successful " + inResponse);
-//                                Toast.makeText(getApplicationContext(), "Payment Transaction is successful ", Toast.LENGTH_LONG).show();
-//                            }
-//
-//                            @Override
-//                            public void onTransactionFailure(String inErrorMessage,
-//                                                             Bundle inResponse) {
-//                                // This method gets called if transaction failed. //
-//                                // Here in this case transaction is completed, but with
-//                                // a failure. // Error Message describes the reason for
-//                                // failure. // Response bundle contains the merchant
-//                                // response parameters.
-//                                Log.d("LOG", "Payment Transaction Failed " + inErrorMessage);
-//                                Toast.makeText(getBaseContext(), "Payment Transaction Failed ", Toast.LENGTH_LONG).show();
-//                            }
-//
-//                            @Override
-//                            public void networkNotAvailable() { // If network is not
-//                                // available, then this
-//                                // method gets called.
-//                            }
-//
-//                            @Override
-//                            public void clientAuthenticationFailed(String inErrorMessage) {
-//                                // This method gets called if client authentication
-//                                // failed. // Failure may be due to following reasons //
-//                                // 1. Server error or downtime. // 2. Server unable to
-//                                // generate checksum or checksum response is not in
-//                                // proper format. // 3. Server failed to authenticate
-//                                // that client. That is value of payt_STATUS is 2. //
-//                                // Error Message describes the reason for failure.
-//                            }
-//
-//                            @Override
-//                            public void onErrorLoadingWebPage(int iniErrorCode,
-//                                                              String inErrorMessage, String inFailingUrl) {
-//
-//                            }
-//
-//                            // had to be added: NOTE
-//                            @Override
-//                            public void onBackPressedCancelTransaction() {
-//                                // TODO Auto-generated method stub
-//                            }
-//
-//                        });
-//            }
-//        });
-
-
-
-    public void onStartTransaction(View view) {
-        PaytmPGService Service = PaytmPGService.getStagingService();
-        Map<String, String> paramMap = new HashMap<String, String>();
-
-        String MID = "WorldP64425807474247";
-        String CUST_ID = "PayAdd123454321678";
-        String INDUSTRY_TYPE = "Retail";
-        String WEBSITE = "worldpressplg";
-        String CHANNEL = "WEB";
-        String AMOUNT = "30";
-        String MOBILE_NO = "7777777777";
-        String THEME = "merchant";
-
-        Random r = new Random(System.currentTimeMillis());
-        String orderId = "ORDER" + (1 + r.nextInt(2)) * 10000
-                + r.nextInt(10000);
-
-        // these are mandatory parameters
-
-        paramMap.put("ORDER_ID", orderId);
-        paramMap.put("MID", MID);
-        paramMap.put("CUST_ID", CUST_ID);
-        paramMap.put("CHANNEL_ID", CHANNEL);
-        paramMap.put("INDUSTRY_TYPE_ID", INDUSTRY_TYPE);
-        paramMap.put("WEBSITE", WEBSITE);
-        paramMap.put("TXN_AMOUNT", AMOUNT);
-        paramMap.put("THEME", THEME);
-        //paramMap.put("EMAIL", EMAIL);
-        paramMap.put("MOBILE_NO", MOBILE_NO);
-        paramMap.put("CALLBACK_URL", "https://pguat.paytm.com/paytmchecksum/paytmCheckSumVerify.jsp");
-        PaytmOrder Order = new PaytmOrder(paramMap);
-
-        PaytmMerchant Merchant = new PaytmMerchant(
-                "https://pguat.paytm.com/paytmchecksum/paytmCheckSumGenerator.jsp",
-                "https://pguat.paytm.com/paytmchecksum/paytmCheckSumVerify.jsp");
-
-        Service.initialize(Order, Merchant, null);
-
-        Service.startPaymentTransaction(this, true, true,
-                new PaytmPaymentTransactionCallback() {
-                    @Override
-                    public void someUIErrorOccurred(String inErrorMessage) {
-                        // Some UI Error Occurred in Payment Gateway Activity.
-                        // // This may be due to initialization of views in
-                        // Payment Gateway Activity or may be due to //
-                        // initialization of webview. // Error Message details
-                        // the error occurred.
-
-                        Log.d("LOG", "Payment Transaction UI Error " + inErrorMessage);
-                        Toast.makeText(getApplicationContext(), "Payment Transaction Ui Error ", Toast.LENGTH_LONG).show();
-                    }
-
-                    @Override
-                    public void onTransactionSuccess(Bundle inResponse) {
-                        // After successful transaction this method gets called.
-                        // // Response bundle contains the merchant response
-                        // parameters.
-                        Log.d("LOG", "Payment Transaction is successful " + inResponse);
-                        Toast.makeText(getApplicationContext(), "Payment Transaction is successful ", Toast.LENGTH_LONG).show();
-                    }
-
-                    @Override
-                    public void onTransactionFailure(String inErrorMessage,
-                                                     Bundle inResponse) {
-                        // This method gets called if transaction failed. //
-                        // Here in this case transaction is completed, but with
-                        // a failure. // Error Message describes the reason for
-                        // failure. // Response bundle contains the merchant
-                        // response parameters.
-                        Log.d("LOG", "Payment Transaction Failed " + inErrorMessage);
-                        Toast.makeText(getBaseContext(), "Payment Transaction Failed ", Toast.LENGTH_LONG).show();
-                    }
-
-                    @Override
-                    public void networkNotAvailable() { // If network is not
-                        // available, then this
-                        // method gets called.
-
-                        Log.d("LOG", "No internet");
-                        Toast.makeText(getApplicationContext(), "Please turn On the Internet!!!", Toast.LENGTH_LONG).show();
-                    }
-
-                    @Override
-                    public void clientAuthenticationFailed(String inErrorMessage) {
-                        // This method gets called if client authentication
-                        // failed. // Failure may be due to following reasons //
-                        // 1. Server error or downtime. // 2. Server unable to
-                        // generate checksum or checksum response is not in
-                        // proper format. // 3. Server failed to authenticate
-                        // that client. That is value of payt_STATUS is 2. //
-                        // Error Message describes the reason for failure.
-                        Log.d("LOG", "Payment Transaction Auth failed " + inErrorMessage);
-                        Toast.makeText(getApplicationContext(), "Payment Auth Failed", Toast.LENGTH_LONG).show();
-                    }
-
-                    @Override
-                    public void onErrorLoadingWebPage(int iniErrorCode,
-                                                      String inErrorMessage, String inFailingUrl) {
-
-                    }
-
-                    // had to be added: NOTE
-                    @Override
-                    public void onBackPressedCancelTransaction() {
-                        // TODO Auto-generated method stub
-                    }
-
-                });
-    }
-
-
 
     @Override
     public void onBackPressed() {
--- app/src/main/java/com/doomers/hackpaytm/ItemDisplayActivity.java
+++ app/src/main/java/com/doomers/hackpaytm/ItemDisplayActivity.java
@@ -2,14 +2,20 @@ package com.doomers.hackpaytm;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class ItemDisplayActivity extends Activity {
 
     private int position;
     private ImageView imageView;
-    private TextView name,price;
+    private TextView name,price,details;
+    TextToSpeech t1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +24,44 @@ public class ItemDisplayActivity extends Activity {
         imageView = (ImageView)findViewById(R.id.imageview);
         name = (TextView)findViewById(R.id.name);
         price = (TextView)findViewById(R.id.price);
+        details = (TextView)findViewById(R.id.further_details);
+
+        details.setText("Promo Code FURN15\n" +
+                "Use promocode FURN15 to get 15% cashback* on total order value. (Max cashback Rs.10000). COD option will not be available on applying this promocode");
 
         position = getIntent().getExtras().getInt("Position");
         switch (position)
         {
-            case 0: imageView.setImageResource(R.drawable.bed); break;
+            case 0: imageView.setImageResource(R.drawable.bed);  break;
             case 1: imageView.setImageResource(R.drawable.table);  name.setText("Rahul Tuteja"); price.setText("$5");break;
             case 2: imageView.setImageResource(R.drawable.sofa); name.setText("Sourabh Suri"); price.setText("$8");break;
-                default: imageView.setImageResource(R.drawable.sofa); break;
+            default: imageView.setImageResource(R.drawable.sofa); break;
         }
+
+        t1=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.UK);
+                }
+            }
+        });
+
+    }
+
+    public void onPause(){
+        if(t1 !=null){
+            t1.stop();
+            t1.shutdown();
+        }
+        super.onPause();
+    }
+
+    public void TextToSpeechCall(View view) {
+
+        String toSpeak = details.getText().toString();
+        Toast.makeText(getApplicationContext(), toSpeak, Toast.LENGTH_SHORT).show();
+        t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
     }
 }
+
